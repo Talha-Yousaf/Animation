360fc74a7c61e542fdeb654177ecaa3f6525b96b
Talha-Yousaf <88709626+Talha-Yousaf@users.noreply.github.com>
2023-04-13 04:11:10 +0500

    update (Talha Yousaf)

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View,FlatList,
+  Pressable,
+  ActivityIndicator, } from 'react-native';
 import { showMessage } from 'react-native-flash-message';
 import { ScreenWrapper } from 'react-native-screen-wrapper';
 import { useDispatch, useSelector } from 'react-redux';
@@ -9,27 +11,15 @@ import { setLoaderVisible } from '../../Redux/Actions/Config';
 import AppColors from '../../utills/AppColors';
 import styles from './styles';
 import AntDesign from "react-native-vector-icons/AntDesign";
-import {width} from "react-native-dimension"
+import {width} from "react-native-dimension";
+import cities from "../../utills/Data"
 export default function Dashboard(props) {
-  const user = useSelector((state) => state.Auth.user);
-  const dispatch = useDispatch();
-  const loginMethod = () => {
-    dispatch(setLoaderVisible(true));
-    setTimeout(() => {
-      showMessage({
-        message: 'Success',
-        description: 'Succfully logged In',
-        type: 'success',
-      });
-      dispatch(setLoaderVisible(false));
-      dispatch(login({ userName: 'John Doe' }));
-    }, 1500);
-  };
+ 
   return (
     <ScreenWrapper statusBarColor={AppColors.white} barStyle='dark-content'>
       <View style={styles.mainViewContainer}>
         <Text> Login</Text>
-        <Button title="Login" onPress={loginMethod} />
+
       </View>
     </ScreenWrapper>
   );
--- src/screens/Login/styles.js
+++ src/screens/Login/styles.js
@@ -2,11 +2,30 @@ import {StyleSheet} from 'react-native';
 import AppColors from '../../utills/AppColors';
 
 const styles = StyleSheet.create({
-  mainViewContainer: {
+  container: {
+    padding: 16,
+    backgroundColor: '#F5F5F5',
+  },
+  city: {
     flex: 1,
+    aspectRatio: 1,
+    marginHorizontal: 8,
+    marginBottom: 16,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 8,
+    elevation: 2,
+    overflow: 'hidden',
     alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: AppColors.white
+  },
+  image: {
+    width: '100%',
+    height: '70%',
+    backgroundColor: 'gainsboro',
+  },
+  name: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 8,
   },
 });
 export default styles;
