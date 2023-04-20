7cd4edf9d9818f37c61723e30383de87d5fd8941
Talha-Yousaf <88709626+Talha-Yousaf@users.noreply.github.com>
2023-04-20 20:16:36 +0500

    update

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -66,7 +66,7 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
     alignSelf:"center"
     // position: 'absolute',
-    // left: width(1),
+    // left: width(1),vhvj
   },
 });
 
