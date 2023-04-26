d8b114de1fa39f2ae0d5bfd7fa828039dca7ef36
Talha-Yousaf <88709626+Talha-Yousaf@users.noreply.github.com>
2023-04-26 18:01:42 +0500

    update

--- src/screens/Dashboard/index.js
+++ src/screens/Dashboard/index.js
@@ -18,6 +18,7 @@ export default function Dashboard(props) {
     });
     dispatch(logout());
   };
+  //dcd
   return (
     <ScreenWrapper statusBarColor={AppColors.white} barStyle="dark-content">
       <View style={styles.mainViewContainer}>
